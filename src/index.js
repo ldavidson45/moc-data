300fa449a3c5b9c5972853963bc9caae0bbbb2e1
Liz Davidson <lizdavidson45@gmail.com>
2020-08-04 14:25:23 -0400

    Add homepage url for gh pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Route, Link, BrowserRouter as Router } from "react-router-dom"
 import "./index.scss"
-import App from "./App"
 
 import CongressPage from "pages/CongressPage"
 import * as serviceWorker from "./serviceWorker"
