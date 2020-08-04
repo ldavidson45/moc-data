0857e295206af3dc255b75a01054788c6c94e341
Liz Davidson <lizdavidson45@gmail.com>
2020-08-04 14:23:33 -0400

    Updates

---
+++ precache-manifest.3fb82e611cfb290e574c542c98750811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568d6b0c665db219bf9fa25a7394936b",
+    "url": "/moc-data/index.html"
+  },
+  {
+    "revision": "e0fac7d71f972a97f294",
+    "url": "/moc-data/static/css/main.8059a3a9.chunk.css"
+  },
+  {
+    "revision": "2fe10bde6375ec24cd89",
+    "url": "/moc-data/static/js/2.a66937dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moc-data/static/js/2.a66937dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0fac7d71f972a97f294",
+    "url": "/moc-data/static/js/main.75b63fd4.chunk.js"
+  },
+  {
+    "revision": "b5386d61d2f80c4d26d0",
+    "url": "/moc-data/static/js/runtime-main.6bc7ebf1.js"
+  }
+]);
